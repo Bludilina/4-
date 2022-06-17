c4b15dbeadca4e548389031dc268086562f7b47f
Bludilina <90140085+Bludilina@users.noreply.github.com>
2022-06-17 07:42:37 +0300

    Add files via upload

---
+++ ConsoleApp4-master/ConsoleApp4-master/ConsoleApp4/person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+
+{
+    internal class person
+    {
+        public person(string name, int year);
+
+
+        public person() { }
+
+        private string name;
+
+        public string Name { get => name; set => name = value; }
+
+
+      
+
+        public override string ToString()
+        {
+            return $"Группа - Персона\n" +
+
+             $"Тип - {Name}\n";
+
+
+        }
+    }
+}
+    
